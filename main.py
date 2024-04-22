25e702e648493dbb3f26fadba9d6bdef84aa68c9
Shivam Arora <shivamarora1@yahoo.com>
2024-04-22 22:03:41 +0530

    added error message

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import time
+import re
+
+
+if "yt_link_txt" not in st.session_state:
+    st.session_state.yt_link_txt = ""
+
+if "side_bar_error" not in st.session_state:
+    st.session_state.side_bar_error = ""
 
 
 def streamed_response_generator(response):
@@ -8,12 +16,32 @@ def streamed_response_generator(response):
         time.sleep(0.05)
 
 
+def is_valid_youtube_url(url):
+    pattern = r'^(https?:\/\/)?(www\.)?(youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})'
+    return bool(re.match(pattern, url))
+
+
+def enter_youtube_link():
+    yt_link = st.session_state['yt_link']
+    if is_valid_youtube_url(yt_link):
+        st.session_state.yt_link_txt = yt_link
+        st.session_state.side_bar_error = ""
+    else:
+        st.session_state.yt_link_txt = ""
+        st.session_state.side_bar_error = "Youtube link is not valid."
+
+
 with st.sidebar:
     st.text_input(label=":red[Youtube] video link", key="yt_link",
                   placeholder="https://www.youtube.com/watch?v=TSzYstGdvDQ")
     st.button(label="Submit", key="submit_yt_link",
-              disabled=False, type="primary")
-    st.video(data="https://www.youtube.com/watch?v=TSzYstGdvDQ")
+              disabled=False, type="primary", on_click=enter_youtube_link)
+
+    if st.session_state.side_bar_error:
+        st.error(st.session_state.side_bar_error)
+
+    if st.session_state.yt_link_txt:
+        st.video(data=st.session_state.yt_link_txt)
 
 st.header("Youtube GPT 🤖")
 st.markdown("##### 🗣️ Converse with 📼 Youtube videos")
